dc35fb8d9bec7c7319ba019f2d7aeef134f7a5a7
Helton <heltonpj5@gmail.com>
2025-11-28 18:18:22 -0300

    feat: Schedule cron task

---
+++ firmware/TODO.md
@@ -0,0 +1,7 @@
+# Todos
+
+- [] Test with ESPAsyncWebServer
+- [] HTMl Templates
+- [] Auth menagment
+- [] Cron timestamp parser
+- [] Dashboard of timers
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,8 +1,11 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
 #include <WebServer.h>
+#include <cronos.hpp>
+#include <esp_sntp.h>
 
 #include <config.h>
+#include <htmlTemplates.h>
 #include <secrets.h>
 
 /*❚█══█❚▬▬ι═══════>-------------------------------------------------------------------------------------------------<═══════ι▬▬❚█══█❚*/
@@ -11,6 +14,14 @@
 #define STOP 90
 
 Servo servo;
+CronoS cron;
+
+const char *ntpServer1 = "pool.ntp.org";
+const char *ntpServer2 = "time.nist.gov";
+
+const char *time_zone = "UTC+3";
+
+const char *cron_schedule = "0 15 7,9,12,15,18,20,22 * * *";
 
 // Wifi configuraton
 WebServer server(80);
@@ -31,38 +42,43 @@ bool isAuthenticated() {
   return false;
 }
 
-void handleFeed() {
-  if (server.method() != HTTP_POST) {
-    server.send(405, "application/json", "{\"error\": \"Wrong method\"}");
-    return;
-  }
-
-  if (!isAuthenticated()) {
-    server.send(401);
-    return;
-  }
-
+void feed(cronos_tid id, void *arg) {
   if (spinning) {
-    server.send(201, "application/json", "{\"msg\": \"Already feeding\"}");
+    Serial.println("Rotina já iniciada");
     return;
   }
 
-  server.send(200, "application/json",
-              "{\"msg\": \"Initializating feeding routing\"}");
   Serial.println("Iniciando a rotina de alimentação");
+  spinning = true;
+
   digitalWrite(BUZZER_PIN, HIGH);
   digitalWrite(LED_PIN, HIGH);
-  spinning = true;
   servo.write(SPIN_R);
 
   delay(2000);
+  spinning = false;
 
   Serial.println("Finalizando a rotina de alimentação");
   servo.write(STOP);
   digitalWrite(BUZZER_PIN, LOW);
   digitalWrite(LED_PIN, LOW);
-  spinning = false;
   return;
+}
+
+void printLocalTime() {
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("No time available (yet)");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+}
+
+void timeavailable(struct timeval *t) {
+  Serial.println("Got time adjustment from NTP!");
+  printLocalTime();
+  // reevaluate rules on time change
+  cron.reload();
 }
 
 /*❚█══█❚▬▬ι═══════>-------------------------------------------------------------------------------------------------<═══════ι▬▬❚█══█❚*/
@@ -87,19 +103,18 @@ void setup() {
   Serial.print("Endereço IP: ");
   Serial.println(WiFi.localIP());
 
-  server.on("/feed", handleFeed);
+  // REST-API
+  // server.on("/feed", HTTP_POST, handleFeed);
+
+  // Default cron jobs
+  configTzTime(time_zone, ntpServer1, ntpServer2);
+
+  sntp_set_time_sync_notification_cb(timeavailable);
+  cron.addCallback(cron_schedule, feed);
 
   server.begin();
+
   Serial.println("Sistema iniciado!");
 }
 
-void loop() {
-
-  server.handleClient();
-  // delay(50);
-  // currentButtonStage = digitalRead(BUTTON_PIN);
-  // if (currentButtonStage != lastButtonStage) {
-  //   handleFeed();
-  //   lastButtonStage = currentButtonStage;
-  // }
-}
+void loop() {}
